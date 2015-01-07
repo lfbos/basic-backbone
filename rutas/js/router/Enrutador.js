f4711142c32a17f4974c1e404a4a8a792e542eca
Luis Boscan <luife642@gmail.com>
2015-01-06 21:37:49 -0430

    Creación de rutas y recuperación de parámetros

--- rutas/js/router/Enrutador.js
+++ rutas/js/router/Enrutador.js
@@ -1,28 +1,58 @@
 var Enrutador = Backbone.Router.extend({
-    routes: {
-        "": "index",
-        "help": "ayuda"
+    routes:{
+        ""                   : "index",
+        "help"               : "ayuda",
+        "panel/:id"          : "muestraPanel",
+        "panel/:id/r:rotulo" : "cambiaPanel"
     },
-    initialize: function() {
+    initialize: function(){
         trace("Aplicando enrutador");
     },
-    index: function() {
+    index: function(){
         $("#help").remove();
-        var PanelesCollection = Backbone.Collection.extend({
-            model: Panel
+         var PanelesCollection = Backbone.Collection.extend({
+             model: Panel
         });
         paneles = new PanelesCollection([
             {txt:"Este es el panel 1", rotulo:"Panel 1", id:"1"},
             {txt:"Este es el panel 2", rotulo:"Panel 2", id:"2"},
             {txt:"Este es el panel 3", rotulo:"Panel 3", id:"3"}
         ]);
-        paneles.on({"add":onChangePanels, "remove":onChangePanels});
+        paneles.on({"add":onChangePanels, "remove":onChangePanels});   
         onChangePanels();
     },
-    ayuda: function() {
-        $("body").append("<div id='help'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a magna quis tortor molestie eleifend. Quisque luctus dignissim nibh vel bibendum. Aenean in quam vulputate, vestibulum velit in, auctor massa. Pellentesque vitae molestie velit. Nam non eros venenatis velit molestie laoreet at ut dolor. Sed pellentesque odio ex, ut bibendum ante viverra ac. Aliquam velit tellus, pretium sed suscipit ac, dapibus vel nisl. Phasellus fringilla, metus eget vestibulum iaculis, metus mi pharetra justo, vel volutpat ligula velit sit amet magna. Praesent vestibulum, tortor maximus condimentum venenatis, augue turpis ultricies purus, et sollicitudin velit mi ac nisl.</div>");
-        $("#help").click(function() {
-            miRouter.navigate("", {trigger: true});
+    ayuda: function(){
+        $("body").append("<div id='help'>Aliquam scelerisque sollicitudin eros a viverra. Maecenas hendrerit urna turpis, facilisis tempor felis iaculis eget. Vivamus sodales felis sem, faucibus auctor ligula eleifend quis. Nullam sit amet lectus nulla. In mattis sodales est, et mattis tellus lacinia eu. Vestibulum vitae vestibulum sapien, ut pharetra mauris. Cras risus quam, varius et congue vitae, euismod a diam. Duis sollicitudin nibh varius justo accumsan, id fermentum libero hendrerit. Phasellus vitae ligula nec turpis molestie eleifend. </div>");
+        $("#help").click(function(){
+         miRouter.navigate("", {trigger:true});
         });
+    },
+    muestraPanel: function(id){
+        var item = paneles.get(id);
+        if(item != undefined){
+            if(panelView != undefined){
+                panelView.undelegateEvents();
+            }
+            $("#contenedor").html("");
+            panelView = new PanelView({el:$("#contenedor"), model:item});
+        }
+    },
+    cambiaPanel: function(id, rotulo) {
+        var item = paneles.get(id);
+
+        if (item != undefined) {
+            item.set("rotulo", rotulo);
+            if (panelView != undefined) {
+                panelView.undelegateEvents();
+            }
+            $("#contenedor").html("");
+            panelView = new PanelView({el: $("#contenedor"), model: item});
+        }
     }
 });
+
+
+
+
+
+
--- rutas/js/rutas.js
+++ rutas/js/rutas.js
@@ -1,23 +1,24 @@
-var paneles;
-var num_paneles;
-var miRouter;
+ var paneles;
+ var num_paneles;
+ var miRouter;
 
 $(document).ready(function(){
-  
-  miRouter = new Enrutador();
+  miRouter = new Enrutador;
   Backbone.history.start();
 
-  $("#enlace_help").click(function() {
-    miRouter.navigate("help", {trigger: true});
-  });
 
-  $("#set_button").click(function() {
+  $("#enlace_help").click(function(){
+    miRouter.navigate("help", {trigger:true});
+  });
+  
+  $("#set_button").click(function(){
     var valor = $("#rot_set").val();
     var id = $(".panel_seleccionado").data("id");
-    if(id != undefined) {
-      
+    if(id != undefined){
+      miRouter.navigate("panel/"+id+"/r"+valor, {trigger:true});
     }
-  });
+});
+
 
   var PanelesCollection = Backbone.Collection.extend({
     model:Panel,
@@ -28,62 +29,15 @@ $(document).ready(function(){
     sortByField: function(campo){
       this.sort_key = campo;
       this.sort();
-    },
+      },
     findByRotulo:function(txt){
       filtered = this.filter(function(item){
         return item.get('rotulo').indexOf(txt) != -1;
       });
       return new PanelesCollection(filtered);
     }
-});
-
-  paneles = new PanelesCollection([
-        {txt:"Este es el panel 1", rotulo:"Panel 1", id:"1"},
-        {txt:"Este es el panel 2", rotulo:"Panel 2", id:"2"},
-        {txt:"Este es el panel 3", rotulo:"Panel 3", id:"3"}
-  ]);
-  onChangePanels(null, null);
-  paneles.on({"add":onChangePanels, "remove":onChangePanels, "reset":onChangePanels, "sort":onChangePanels});
-  trace(JSON.stringify(paneles.toJSON()));
-  num_paneles = paneles.length + 1;
-    
-  $("#create_button").click(function(){
-    /*var panel = new Panel({txt:"Este es el panel 4", rotulo:"Panel 4", id:paneles.length});
-    paneles.add(panel);*/
-
-    paneles.add({txt:"Este es el panel " + num_paneles, rotulo:num_paneles + "Panel " + num_paneles, id:num_paneles});
-    num_paneles ++;
-  });
-
-  $("#delete_button").click(function(){
-    paneles.remove(paneles.at(0));
-  });
-
-  $("#delete_button_ID").click(function(){
-    paneles.remove(paneles.get($("#rot_del").val()));
-  });
-
-  $("#reset_button").click(function(){
-    paneles.reset();
-  });
-
-  $("#set_button").click(function(){
-      var _id = $(".panel_seleccionado").data("id");
-      var item = paneles.get(_id);
-      item.set("rotulo", $("#rot_set").val());
-  });
-
-  $("#sort_button").click(function(){
-    paneles.sortByField("rotulo");
-  });
-
-  $("#filter_button").click(function(){
-    trace(paneles.findByRotulo($("#rot_filter").val()));
-    trace(paneles.where({"rotulo": $("#rot_filter").val()}));
   });
 });
-
-
 
 
 function onChangePanels(model, collection){
@@ -91,7 +45,7 @@ function onChangePanels(model, collection){
     //paneles.each(pintaPanel);
     var botonView = new BotonView({el:$("#listado ul"), collection:paneles});
 }
-    
+
 //función que escribe un mensaje en la consola
 function trace(msg){
   $("#consola").append(msg+"<br>");
